16f88824c9a43387a26c37064a983e7fbc83cb58
lmh05k68 <158491940+lmh05k68@users.noreply.github.com>
2024-05-26 10:18:16 +0700

    Delete CIJS96 directory

--- CIJS96/lesson 1/bai 1.js
+++
@@ -1,7 +0,0 @@
-function BinhPhuong() {
-    let userInput = prompt("Vui lòng nhập vào một số:");
-    let number = parseFloat(userInput);
-    let square = number * number;
-    console.log( square);
-}
-BinhPhuong();
--- CIJS96/lesson 1/bai 2.js
+++
@@ -1,4 +0,0 @@
-let bai2 = (text, word) => {
-    return text.includes(word)
-}
-console.log(bai2("Hello world", "world"));
